0f4c2afb86abbaf80f749898e485bc6fab112dd9
Sermpezis <emmanouil.sermpezis@mvi-hellas.com>
2022-12-10 19:46:23 +0100

    board structure

--- src/main/java/org/example/BoardResource.java
+++ src/main/java/org/example/BoardResource.java
@@ -6,4 +6,16 @@ public class BoardResource implements ICharacters {
     private CharacterResource[] secondPlayerCharacters = ICharacters.characters;
     private int turn = 1;
 
+
+    public void showBoard(){
+        System.out.println("Player 1 remaining characters:");
+        for ( CharacterResource character : firstPlayerCharacters){
+            if (character.)
+            System.out.println(character.getCharacterName());
+        }
+        System.out.println("Player 2 remaining characters:");
+        for ( CharacterResource character : secondPlayerCharacters){
+            System.out.println(character.getCharacterName());
+        }
+    }
 }
